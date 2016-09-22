166e5430a85c99610c1346186d9b38b880851f30
Enerdy <rodrigo.rente@live.com>
2016-09-23 00:30:48 +0100

    Add project files.

---
+++ HighlightBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighlightBot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
